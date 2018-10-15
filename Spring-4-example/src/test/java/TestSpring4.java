8ac91e1944164a6e8165f77c68eb0d5f00632392
Andrew Marshall <andrew.marshall@invosys.com>
2018-10-15 11:36:44 +0100

    Test

---
+++ Spring-4-example/src/test/java/TestSpring4.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.websystique.spring.AppMain;
+
+public class TestSpring4 {
+
+	@Test
+	public void test() {
+		String[] args = null;
+		AppMain.main(args);
+	
+	}
+
+}
